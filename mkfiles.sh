bad23459d82f21744d7e3ef5ad0860d2c2e90bf4
Stuart Longland <me@vk4msl.id.au>
2019-08-17 14:01:08 +1000

    Move to Debian Buster image for package builds.

--- mkfiles.sh
+++ mkfiles.sh
@@ -2,7 +2,7 @@
 
 # Generate the Dockerfiles
 
-: ${BASE_IMAGES:=debian:stretch-slim ubuntu:bionic}
+: ${BASE_IMAGES:=debian:buster-slim ubuntu:bionic}
 
 for IMAGE in ${BASE_IMAGES}; do
 	DOCKERFILE=Dockerfile.$( echo ${IMAGE} | tr : - )
