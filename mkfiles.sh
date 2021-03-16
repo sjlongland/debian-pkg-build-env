0b5ec7b733d4761c79e0c9f7065e33e1313a7a50
Stuart Longland <me@vk4msl.id.au>
2021-03-16 20:33:26 +1000

    Bump Ubuntu to 20.04

--- mkfiles.sh
+++ mkfiles.sh
@@ -2,7 +2,7 @@
 
 # Generate the Dockerfiles
 
-: ${BASE_IMAGES:=debian:buster-slim ubuntu:bionic}
+: ${BASE_IMAGES:=debian:buster-slim ubuntu:focal}
 
 for IMAGE in ${BASE_IMAGES}; do
 	DOCKERFILE=Dockerfile.$( echo ${IMAGE} | tr : - )
